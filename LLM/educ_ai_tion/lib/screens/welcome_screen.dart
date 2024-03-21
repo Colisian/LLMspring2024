6deb3bb240507c01ac9403d8eff33ec9cec5ae9f
mss-code <54789804+mss-code@users.noreply.github.com>
2024-03-21 17:22:46 -0400

    starting to put code in stack

--- LLM/educ_ai_tion/lib/screens/welcome_screen.dart
+++ LLM/educ_ai_tion/lib/screens/welcome_screen.dart
@@ -9,121 +9,139 @@ class WelcomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Educ_ai_tion'),
+        title: Text('Educ-AI-tion'),
       ),
-      body: SingleChildScrollView(
-        // Wrap with SingleChildScrollView for vertical scrolling
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // First Row
-              SizedBox(
-                width: screenWidth * 0.9, // 90% of screen width
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    // First Column
-                    SizedBox(
-                      width: screenWidth * 0.4, // 40% of screen width
-                      child: Column(
-                        children: <Widget>[
-                          Image.asset('assets/images/home_image_top.PNG'),
-                        ],
+      body: SafeArea(
+              child: SingleChildScrollView( child: Stack()
+                
+                
+          // Wrap with SingleChildScrollView for vertical scrolling
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 100), // Spacer between rows
+                // First Row
+                SizedBox(
+                  width: screenWidth * 0.9, // 90% of screen width
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      // First Column
+                      SizedBox(
+                        width: screenWidth * 0.4, // 40% of screen width
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          //logo------------------------------------------------
+                          children: <Widget>[
+                            Image.asset('assets/images/logo.PNG'),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20), // Spacer between rows
-              // Second Row
+                const SizedBox(height: 20), // Spacer between rows
+                // Second Row
 
-              SizedBox(
-                width: screenWidth * 0.9, // 90% of screen width
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    // First Column
-                    SizedBox(
-                      width: screenWidth * 0.4, // 40% of screen width
-                      child: Column(
-                        children: <Widget>[
-                          Image.asset(
-                              'assets/images/home_image_empTeacher.PNG'),
-                        ],
+                SizedBox(
+                  width: screenWidth * 0.9, // 90% of screen width
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      // First Column
+                      SizedBox(
+                        width: screenWidth * 0.4, // 40% of screen width
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset(
+                                'assets/images/home_image_empTeacher.PNG'),
+                          ],
+                        ),
                       ),
-                    ),
-                    // Second Column
-                    SizedBox(
-                      width: screenWidth * 0.4, // 40% of screen width
-                      child: const Column(
-                        children: <Widget>[
-                          Text('Empowering Teachers'),
-                          Text(
-                              'Efficient course planning tools to save time for teachers..Efficient course planning tools to save time for teachers..Efficient course planning tools to save time for teachers..Efficient course planning tools to save time for teachers..Efficient course planning tools to save time for teachers..'),
-                        ],
+                      // Second Column
+                      SizedBox(
+                        width: screenWidth * 0.4, // 40% of screen width
+                        child: const Column(
+                          children: <Widget>[
+                            Text('Welcome',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  color: Color(0xFFE4AEF2),
+                                )),
+                            Text('The Ultimate AI Assistant in Academia',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Color(0xFFE4AEF2),
+                                )),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20), // Spacer between rows
-              // third Row
+                const SizedBox(height: 20), // Spacer between rows
+                // third Row
 
-              SizedBox(
-                width: screenWidth * 0.9, // 90% of screen width
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    // First Column
-                    SizedBox(
-                      width: screenWidth * 0.4, // 40% of screen width
-                      child: const Column(
-                        children: <Widget>[
-                          Text('Create Interactive Quizzes and Tests'),
-                          Text(
-                              'Efficient course planning tools to save time for teachers..Efficient course planning tools to save time for teachers..Efficient course planning tools to save time for teachers..Efficient course planning tools to save time for teachers..Efficient course planning tools to save time for teachers..'),
-                        ],
+                SizedBox(
+                  width: screenWidth * 0.9, // 90% of screen width
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      // First Column
+                      SizedBox(
+                        width: screenWidth * 0.5, // 40% of screen width
+                        child: const Column(
+                          children: <Widget>[
+                            Text(
+                                'Helping instructors harness the power of LLMs in education to provide individualized instruction that fosters deep learning in a revolutionary way.',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Color(0xFFE4AEF2),
+                                )),
+                            Text('Efficieve time for teachers..'),
+                          ],
+                        ),
                       ),
-                    ),
-                    // Second Column
-                    SizedBox(
-                      width: screenWidth * 0.4, // 40% of screen width
-                      child: Column(
-                        children: <Widget>[
-                          Image.asset(
-                              'assets/images/home_image_createInteractive.PNG'),
-                        ],
+                      // Second Column
+                      SizedBox(
+                        width: screenWidth * 0.4, // 40% of screen width
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset(
+                                'assets/images/home_image_createInteractive.PNG'),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20), // Spacer between rows
-              // fourth Row
-              SizedBox(
-                width: screenWidth * 0.9, // 90% of screen width
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          // Navigate to student login page
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => LoginScreen()),
-                        );
-                      },
-                      child: const Text('login to ESS'),
-                    ),
-                  ],
+                const SizedBox(height: 20), // Spacer between rows
+                // fourth Row
+                SizedBox(
+                  width: screenWidth * 0.9, // 90% of screen width
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              // Navigate to student login page
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginScreen()),
+                            );
+                          },
+                          child: const Text('login to ESS'),
+                          style: ElevatedButton.styleFrom()),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
+
     );
   }
 }
